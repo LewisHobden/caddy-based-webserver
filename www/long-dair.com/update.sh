6f5f95a2576ed4c1433c909c4f54130ccb2d5c40
Lewis Hobden <lewis@hobden.xyz>
2021-05-19 18:53:26 +0000

    Functional test.

--- www/long-dair.com/update.sh
+++ www/long-dair.com/update.sh
@@ -1,1 +1,1 @@
-docker run --env-file config/.env --rm -v /srv/www/long-dair.com/html:/app/out tyrrrz/discordchatexporter:stable export -c $EXPORT_CHANNEL_ID -b -o index.html
+docker run --env-file /srv/www/config/.env --rm -v /srv/www/long-dair.com/html:/app/out tyrrrz/discordchatexporter:stable export -c $EXPORT_CHANNEL_ID -b -o index.html
